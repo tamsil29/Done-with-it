d27b07f804cb7f70d0b9f438f93d5a4e71c42775
tamsil <tamsilansari29@gmail.com>
2023-05-12 22:03:57 +0530

    added uselocation hook

--- app/screens/ListingEditScreen.tsx
+++ app/screens/ListingEditScreen.tsx
@@ -1,41 +1,29 @@
 import React, { useEffect, useState } from "react";
 import * as Yup from "yup";
-import { StyleSheet } from 'react-native'
+import { StyleSheet } from "react-native";
 import { Form, FormField, FormPicker, SubmitButton } from "../components/forms";
 import Screen from "../components/Screen";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/forms/FormImagePicker";
-import * as Location from 'expo-location'
+import useLocation from "../hooks/useLocation";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
-  images: Yup.array().required().min(1,"Please select atleast one image!")
-
+  images: Yup.array().required().min(1, "Please select atleast one image!"),
 });
 
 const categories = [
-  { label: "Furniture & mo", value: 1, backgroundColor:'red', icon:'apps' },
-  { label: "Clothing", value: 2, backgroundColor:'green', icon:'email' },
-  { label: "Camera", value: 3, backgroundColor:'blue', icon:'lock' },
+  { label: "Furniture & mo", value: 1, backgroundColor: "red", icon: "apps" },
+  { label: "Clothing", value: 2, backgroundColor: "green", icon: "email" },
+  { label: "Camera", value: 3, backgroundColor: "blue", icon: "lock" },
 ];
 
 function ListingEditScreen() {
-  const [location, setLocation] = useState({} as any)
-  useEffect(() => {
-   getLocation()
-  }, [])
+  const { location } = useLocation();
 
-  const getLocation = async () => {
-    const {granted} = await Location.requestForegroundPermissionsAsync();
-    if(!granted) return;
-    const {coords: {latitude, longitude}} = await Location.getLastKnownPositionAsync();
-    setLocation({latitude, longitude})
-
-  }
-  
   return (
     <Screen style={styles.container}>
       <Form
@@ -44,12 +32,12 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
-          images: []
+          images: [],
         }}
         onSubmit={(values) => console.log(location)}
         validationSchema={validationSchema}
       >
-        <FormImagePicker name={"images"}/>
+        <FormImagePicker name={"images"} />
         <FormField maxLength={255} name="title" placeholder="Title" />
         <FormField
           keyboardType="numeric"
@@ -63,7 +51,7 @@ function ListingEditScreen() {
           name={"category"}
           PickerItemComponent={CategoryPickerItem}
           placeholder={"Category"}
-          width={'50%'}
+          width={"50%"}
           numberOfColumns={3}
         />
         <FormField
@@ -73,18 +61,18 @@ function ListingEditScreen() {
           name="description"
           placeholder="Description"
         />
-        <SubmitButton title={"Post"}/>
+        <SubmitButton title={"Post"} />
       </Form>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        gap: 20,
-        marginTop:20,
-        marginHorizontal: 10,
-      },
-})
+  container: {
+    gap: 20,
+    marginTop: 20,
+    marginHorizontal: 10,
+  },
+});
 
 export default ListingEditScreen;
