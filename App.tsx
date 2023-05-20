f65db54104fd4d0eaa455c4c7925317403fd8f18
tamsil <tamsilansari29@gmail.com>
2023-05-20 20:29:26 +0530

    splash screen handling

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import * as SplashScreen from "expo-splash-screen";
+
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
@@ -8,24 +10,42 @@ import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 import jwtDecode from "jwt-decode";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
+  const [appIsReady, setAppIsReady] = useState(false);
 
   const restoreToken = async () => {
-    const token = await authStorage.getToken()
-    if(!token) return;
-    setUser(jwtDecode(token))
-  }
+    try {
+      const token = await authStorage.getToken();
+      if (token) setUser(jwtDecode(token));
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setAppIsReady(true);
+    }
+  };
 
   useEffect(() => {
-    restoreToken()
-  },[])
+    restoreToken();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
-    <AuthContext.Provider value={{user, setUser}}>
+    <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
-      <NavigationContainer theme={navigationTheme}>
-        {user ? <AppNavigator /> :  <AuthNavigator/>}
+      <NavigationContainer theme={navigationTheme} onReady={onLayoutRootView}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </AuthContext.Provider>
   );
