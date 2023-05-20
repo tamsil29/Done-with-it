01697dda83ff133f00236f402c8aacba49fda632
tamsil <tamsilansari29@gmail.com>
2023-05-20 16:15:23 +0530

    added cache layer

---
+++ app/utility/cache.ts
@@ -0,0 +1,40 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { differenceInMinutes } from 'date-fns';
+
+
+const prefix = 'cache'
+const expiryInMinutes = 5
+
+const store = async (key: string, value: any) => {
+    try {
+        const item = {
+            value,
+            timeStamp: Date.now()
+        }
+        await AsyncStorage.setItem(prefix + key, JSON.stringify(item));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const get = async (key: string) => {
+    try {
+        const value = await AsyncStorage.getItem(prefix + key);
+        const item = JSON.parse(value as string)
+
+        if(!item) return null;
+
+        const isExpired = differenceInMinutes(Date.now(), item?.timeStamp) > expiryInMinutes
+        if(isExpired) {
+            await AsyncStorage.removeItem(prefix + key)
+            return null
+        }
+
+        return item.value
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default store 
