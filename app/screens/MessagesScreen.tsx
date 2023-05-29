77f3cc5d7136aeb906772727b54955f65323a36d
tamsil <tamsilansari29@gmail.com>
2023-05-29 15:02:50 +0530

    integrated api with messages screen

--- app/screens/MessagesScreen.tsx
+++ app/screens/MessagesScreen.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import Icon from "../components/Icon";
 import ListItem from "../components/ListItem";
 import ListItemDeleteAction from "../components/ListItemDeleteAction";
 import ListItemSeparator from "../components/ListItemSeparator";
 import Screen from "../components/Screen";
+import useApi from "../hooks/useApi";
+import messagesApi from '../api/message'
+import useAuth from "../auth/useAuth";
 
 const _messages = [
   {
@@ -24,37 +27,43 @@ const _messages = [
 ];
 
 function MessagesScreen() {
+  const image = "https://wallpapers.com/images/featured-full/cool-profile-pictures-4co57dtwk64fb7lv.jpg"
+  const { data, request: getConvos, isLoading} = useApi(messagesApi.getConversations);
+  const {user} = useAuth()
   const [messages, setMessages] = useState(_messages)
   const [refreshing, setRefreshing] = useState(false)
   const handleDelete = (message: any) =>{
     setMessages(messages.filter(msg => msg.id !== message.id))
   }
 
+  useEffect(()=>{
+    getConvos()
+  }, [])
+
   return (
     <Screen style={styles.container}>
       <FlatList
         ItemSeparatorComponent={ListItemSeparator}
-        data={messages}
-        keyExtractor={(message) => message.id.toString()}
+        data={data}
+        keyExtractor={(messages) => messages._id.toString()}
         renderItem={({ item }) => (
           <ListItem
-            title={item.title}
-            subTitle={item.description}
-            image={item.image}
+            title={item.userId1 === user._id ? item.user2Data.name : item.user1Data.name}
+            subTitle={item?.recentMessage?.message}
+            image={image}
             onPress={() => console.log(123)}
             renderRightActions={()=><ListItemDeleteAction onPress={()=>handleDelete(item)}/>}
           />
         )}
-        refreshing={refreshing}
-        onRefresh={()=>setMessages([..._messages])}
+        refreshing={isLoading}
+        onRefresh={getConvos}
       />
-      <ListItem
+      {/* <ListItem
             title={'My item'}
 
             onPress={() => console.log(123)}
             IconComponent={<Icon name="email"/>}
-          />
-      
+          /> */}
     </Screen>
   );
 }
