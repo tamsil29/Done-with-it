f1f39ebd21e77297c3ccb8d0a43a0b35c751db45
tamsil <tamsilansari29@gmail.com>
2023-06-01 15:18:36 +0530

    added self api

--- app/api/users.ts
+++ app/api/users.ts
@@ -2,4 +2,6 @@ import client from './client'
 
 const register = (userInfo: {name: string, email: string, password: string}) => client.post<any>('/users', userInfo)
 
-export default { register }
+const getMe = () => client.get<any>('/me')
+
+export default { register, getMe }
--- app/auth/useAuth.ts
+++ app/auth/useAuth.ts
@@ -2,9 +2,12 @@ import { useContext } from "react";
 import AuthContext from "./context";
 import authStorage from "./storage";
 import jwtDecode from "jwt-decode";
+import usersApi from "../api/users";
+import useApi from "../hooks/useApi";
 
 const useAuth = () => {
   const { user, setUser } = useContext(AuthContext);
+  const getSelf = useApi(usersApi.getMe)
 
   const logIn = (token: string) => {
     const user = jwtDecode(token);
@@ -12,12 +15,19 @@ const useAuth = () => {
     authStorage.storeToken(token);
   }
 
+  const updateSelf = async (user?:any) => {
+    if(user) return setUser(user);
+
+    const result = await getSelf.request();
+    if(result.ok) setUser(result?.data?.data);
+  }
+
   const logOut = async () => {
     setUser(null);
     await authStorage.removeToken();
   };
 
-  return { user, setUser, logOut, logIn };
+  return { user, setUser, logOut, logIn, updateUser: updateSelf };
 };
 
 export default useAuth;
