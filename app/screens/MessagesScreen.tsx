ca62f69cfdf9bd24809e8f27127153a104aa6b6b
tamsil <tamsilansari29@gmail.com>
2023-06-01 14:51:49 +0530

    userimage fix

--- app/screens/MessagesScreen.tsx
+++ app/screens/MessagesScreen.tsx
@@ -6,10 +6,11 @@ import ListItemDeleteAction from "../components/ListItemDeleteAction";
 import ListItemSeparator from "../components/ListItemSeparator";
 import Screen from "../components/Screen";
 import useApi from "../hooks/useApi";
-import messagesApi from '../api/message'
+import messagesApi from "../api/message";
 import useAuth from "../auth/useAuth";
 import useRouteNavigation from "../hooks/useRouteNavigation";
 import NoData from "../components/NoData";
+import { getUserImage } from "../utility/utilities";
 
 const _messages = [
   {
@@ -29,34 +30,47 @@ const _messages = [
 ];
 
 function MessagesScreen() {
-  const image = "https://wallpapers.com/images/featured-full/cool-profile-pictures-4co57dtwk64fb7lv.jpg"
-  const { data, request: getConvos, isLoading} = useApi(messagesApi.getConversations);
-  const {user} = useAuth()
-  const navigation = useRouteNavigation()
-  const [messages, setMessages] = useState(_messages)
-  const [refreshing, setRefreshing] = useState(false)
-  const handleDelete = (message: any) =>{
-    setMessages(messages.filter(msg => msg.id !== message.id))
-  }
+  const image =
+    "https://wallpapers.com/images/featured-full/cool-profile-pictures-4co57dtwk64fb7lv.jpg";
+  const {
+    data,
+    request: getConvos,
+    isLoading,
+  } = useApi(messagesApi.getConversations);
+  const { user } = useAuth();
+  const navigation = useRouteNavigation();
+  const [messages, setMessages] = useState(_messages);
+  const [refreshing, setRefreshing] = useState(false);
+  const handleDelete = (message: any) => {
+    setMessages(messages.filter((msg) => msg.id !== message.id));
+  };
 
-  useEffect(()=>{
-    getConvos()
-  }, [])
+  useEffect(() => {
+    getConvos();
+  }, []);
+
+  const getOtherUser = (user1Data: any, user2Data: any) => {
+    return user1Data?._id === user._id ? user2Data : user1Data;
+  };
 
   return (
     <Screen style={styles.container}>
-      {data && data.length === 0 && <NoData value='messages'/>}
+      {data && data.length === 0 && <NoData value="messages" />}
       <FlatList
         ItemSeparatorComponent={ListItemSeparator}
         data={data}
         keyExtractor={(messages) => messages._id.toString()}
         renderItem={({ item }) => (
           <ListItem
-            title={item.userId1 === user._id ? item.user2Data.name : item.user1Data.name}
+            title={getOtherUser(item.user1Data, item.user2Data)?.name}
             subTitle={item?.recentMessage?.message}
-            image={image}
-            onPress={() => navigation.navigate('Chat', item)}
-            renderRightActions={()=><ListItemDeleteAction onPress={()=>handleDelete(item)}/>}
+            image={getUserImage(
+              getOtherUser(item.user1Data, item.user2Data)?.imageId
+            )}
+            onPress={() => navigation.navigate("Chat", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
           />
         )}
         refreshing={isLoading}
@@ -73,9 +87,9 @@ function MessagesScreen() {
 }
 
 const styles = StyleSheet.create({
-  container:{
-    paddingTop: 0
-  }
+  container: {
+    paddingTop: 0,
+  },
 });
 
 export default MessagesScreen;
