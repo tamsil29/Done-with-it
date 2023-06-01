866219ecad47a3ab886c04f78c58d2b6f4027686
tamsil <tamsilansari29@gmail.com>
2023-06-02 01:00:35 +0530

    wip view image screen

--- app/screens/ViewImageScreen.tsx
+++ app/screens/ViewImageScreen.tsx
@@ -1,33 +1,32 @@
 import React from "react";
-import {
-  View,
-  Image,
-  StyleSheet,
-  TouchableOpacity
-} from "react-native";
+import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
 import useRouteNavigation from "../hooks/useRouteNavigation";
 
-function ViewImageScreen(props: any) {
+function ViewImageScreen() {
   const navigation = useRouteNavigation();
   const img =
     "https://aarsunwoods.b-cdn.net/wp-content/uploads/2020/03/Sofa-Chair-for-Luxury-Home-UH-FP-0019.jpg";
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => console.log(12345)}>
-        <View style={styles.closeIcon}>
-          <MaterialCommunityIcons name="close" color="white" size={30} />
-        </View>
-      </TouchableOpacity>
-      <View style={styles.deleteIcon}>
-        <MaterialCommunityIcons
-          name="trash-can-outline"
-          color="white"
-          size={30}
-        />
-      </View>
+      <MaterialCommunityIcons
+        name="close"
+        color="white"
+        size={30}
+        style={styles.closeIcon}
+        onPress={() => navigation.goBack()}
+      />
+
+      <MaterialCommunityIcons
+        name="trash-can-outline"
+        color="white"
+        size={30}
+        onPress={() => console.log(1234)}
+        style={styles.deleteIcon}
+      />
+
       <Image style={styles.image} resizeMode="contain" source={{ uri: img }} />
     </View>
   );
@@ -38,6 +37,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 40,
     left: 30,
+    zIndex: 1,
   },
   container: {
     backgroundColor: colors.black,
@@ -46,6 +46,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 40,
     right: 30,
+    zIndex: 1,
   },
   image: {
     height: "100%",
