cc5bbdcdd2bd7a1fa601b39d3d472666c7208d67
tamsil <tamsilansari29@gmail.com>
2023-06-01 14:45:30 +0530

    added default avatar

--- app/utility/utilities.ts
+++ app/utility/utilities.ts
@@ -2,8 +2,10 @@ export function formatPrice(price: number) {
   return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+const defaultUserAvatar = 'https://w7.pngwing.com/pngs/717/24/png-transparent-computer-icons-user-profile-user-account-avatar-heroes-silhouette-black-thumbnail.png'
+
 export function getUserImage(imageObject: any) {
   return imageObject?.baseUrl && imageObject?.key
     ? imageObject?.baseUrl + imageObject?.key
-    : "https://shorturl.at/qFGY8";
+    : defaultUserAvatar;
 }
